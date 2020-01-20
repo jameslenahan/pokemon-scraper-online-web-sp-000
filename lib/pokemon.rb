c9b7c5923df4f707ff2fe38c65749c8032224b74
James Lenahan <james.k.lenahan@gmail.com>
2020-01-20 15:07:21 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,4 +11,7 @@ class Pokemon
     @@all << self
   
   end
+  def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
 end
